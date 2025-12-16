e99398e584b9e6166aaff256d9a7392c2de23dcf
Udezi Oghenero <udezioghenero@mail.com>
2025-12-16 11:22:46 +0100

    remove node modules from git tracking

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+
+app.use(cors());
+
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+});
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
