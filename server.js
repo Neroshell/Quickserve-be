56ed2a98d9b8e8d15f5cc3e7ce3967b92115c3d2
neroshell <nero@marketingpark.org>
2026-02-22 12:30:43 +0100

    Add frontend base url

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: process.env.FRONTEND_BASE_URL || "http://localhost:3000",
+  credentials: true,
+}));
 
 // Routes
 app.use("/orders", orderRoute)
