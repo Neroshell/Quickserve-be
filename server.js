24873d671437ecb2e6ca69bbd9fc900de552cfa4
neroshell <nero@marketingpark.org>
2026-02-22 12:41:22 +0100

    fix cors

--- server.js
+++ server.js
@@ -16,12 +16,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(express.json())
-// app.use(cors())
-
-app.use(cors({
-  origin: process.env.FRONTEND_BASE_URL || "http://localhost:3000",
-  credentials: true,
-}));
+app.use(cors())
 
 // Routes
 app.use("/orders", orderRoute)
